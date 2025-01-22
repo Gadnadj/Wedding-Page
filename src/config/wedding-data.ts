ba29efb37f6eb86f3495c4331845d5f74353c836
rudy002 <rudyhaddad.job@gmail.com>
2025-01-22 18:25:39 +0200

    Update wedding location details and map link in wedding-data.ts

--- src/config/wedding-data.ts
+++ src/config/wedding-data.ts
@@ -6,14 +6,14 @@ export const weddingData = {
   date: "27.08.2025",
   location: {
     fr: {
-      name: "Château des Fleurs",
-      address: "123 rue des Roses, Paris",
+      name: "Destino",
+      address: "Destino, Ha-Pisga St, Shoresh, Israel",
     },
     he: {
-      name: "שאטו דה פלר",
-      address: "123 רחוב הוורדים, פריז",
+      name: "דסטינו",
+      address: "דסטינו, שושן ברכה, ישראל",
     },
-    mapLink: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2624.9916256937586!2d2.2922926156740714!3d48.858370079287475!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66e2964e34e2d%3A0x8ddca9ee380ef7e0!2sTour%20Eiffel!5e0!3m2!1sfr!2sfr!4v1647951825714!5m2!1sfr!2sfr",
+    mapLink: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1615.244390195324!2d35.066619787814616!3d31.795080287802865!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1502d06ef3bd8581%3A0xdd0a666f25ed0666!2sDestino!5e0!3m2!1sfr!2sil!4v1737563033970!5m2!1sfr!2sil",
   },
   schedule: {
     fr: [
